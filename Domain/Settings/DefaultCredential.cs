ed5ec7c865a33045859fa83e1a7aa965aa619e23
Võ Trung Thường <hothy91@gmail.com>
2021-12-05 23:38:01 +0700

    add seed sample data method

--- Domain/Settings/DefaultCredential.cs
+++ Domain/Settings/DefaultCredential.cs
@@ -2,6 +2,8 @@
 {
     public class DefaultCredential
     {
+        public Guid AdminId { get; set; }
+        public Guid AdminRoleId { get; set; }
         public string? DefaultPassword { get; set; }
     }
 }
